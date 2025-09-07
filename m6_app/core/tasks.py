97ef7b797d9d73465d4e635a08b2fabbbab6fd81
JojoBaPb <sipo.jmudenda@gmail.com>
2025-09-07 19:51:17 +0200

    render ready commit

--- m6_app/core/tasks.py
+++ m6_app/core/tasks.py
@@ -1,6 +1,12 @@
 from celery import shared_task
+from django.core.mail import send_mail
 
 @shared_task
 def add(x, y):
     return x + y
+
+@shared_task
+def send_test_email(to_email):
+    send_mail('Hello from m6_app', 'This is a test', None, [to_email], fail_silently=False)
+    return True
 
