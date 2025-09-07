355e0a6c9b25cc62539b538e1f16ee70c0a097fb
JojoBaPb <sipo.jmudenda@gmail.com>
2025-09-07 20:35:08 +0200

    prod fixes

--- m6_app/m6_app/asgi.py
+++ m6_app/m6_app/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'm6_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "m6_app.m6_app.settings")
 
 application = get_asgi_application()
--- m6_app/manage.py
+++ m6_app/manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'm6_app.settings')
-    try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "m6_app.m6_app.settings")
+   try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
